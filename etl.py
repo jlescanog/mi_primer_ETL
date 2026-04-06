3b6ebd82c94fdaf9da2a9be1d150e0ce392cf848
jlescanog <jlescanoguevara@gmail.com>
2026-04-06 00:06:38 -0500

    Añadir manejo de valores nulos en df_orders: eliminación y relleno de datos

--- etl.py
+++ etl.py
@@ -31,3 +31,39 @@ print("\n🔍 Primeras filas de orders:")
 print(df_orders.head())
 print("\n📋 Info de orders:")
 print(df_orders.info())
+
+# Identificando valores nulos
+
+print("\n❌ Valores nulos:")
+print(df_orders.isnull().sum())
+print(df_order_items.isnull().sum())
+print(df_customers.isnull().sum())
+print(df_products.isnull().sum())
+
+# Valores nulos solo en:
+# df_orders
+# promotion_id        3784
+# notes               4188
+
+#Ejemplos: eliminando filas con nulos df.dropna
+df_orders_elimnando = df_orders.dropna(subset=['promotion_id', 'notes'])
+
+#Ejemplo: rellenando valores nulos con un valor específico df.fillna
+df_orders_rellenando = df_orders.fillna({'promotion_id': 0, 'notes': 'Sin información'})
+
+print(f"Filas antes: {len(df_orders)}")
+print(f"Filas después (eliminando nulos): {len(df_orders_elimnando)}")
+print(f"Filas después (rellenando nulos): {len(df_orders_rellenando)}")
+
+# Salida:
+# Filas antes: 5000
+# Filas después (eliminando nulos): 199
+# Filas después (rellenando nulos): 5000
+
+#Rellenamos con 0 para no dismiuir el tamaño del dataset!!
+
+df_orders = df_orders_rellenando.copy()
+print(df_orders.isnull().sum())
+
+
+
